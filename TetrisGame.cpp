b7d65d4c622aa8d90f96807ff9af860a324dc10c
Shubham <khalkhoshubham03@gmail.com>
2025-01-06 19:53:32 -0700

    Add initial Tetris project files

--- TetrisGame.cpp
+++ TetrisGame.cpp
@@ -4,10 +4,15 @@ int main()
 {
     InitWindow(600, 900, "Tetris Game");
     SetTargetFPS(120);
+    Texture2D background = LoadTexture("background.jpg");
+
     while(WindowShouldClose() == false)
     {
         BeginDrawing();
+        DrawTexture(background,-50,-60,WHITE);
         EndDrawing();
     }
+    UnloadTexture(background);
     CloseWindow();
+    return 0;
 }
